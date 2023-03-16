05c4b250ec4994cd56c186def201ed38d6e79fd4
Daniel Vilardell Regué <danielvilardellregue@Daniels-MacBook-Pro.local>
2023-03-16 10:26:54 +0100

    SC generation for plonk

---
+++ sc_plonk/generate_sc_plonk.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"os"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/plonk"
+	"github.com/consensys/gnark/examples/cubic"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/test"
+)
+
+func generatePlonk() error {
+	var circuit cubic.Circuit
+
+	scs, err := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, &circuit)
+	if err != nil {
+		return err
+	}
+
+	srs, err := test.NewKZGSRS(scs)
+	if err != nil {
+		return err
+	}
+	{
+		f, err := os.Create("kzg.plonk.srs")
+		if err != nil {
+			return err
+		}
+		_, err = srs.WriteTo(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	pk, vk, err := plonk.Setup(scs, srs)
+	if err != nil {
+		return err
+	}
+	{
+		f, err := os.Create("cubic.plonk.vk")
+		if err != nil {
+			return err
+		}
+		_, err = vk.WriteTo(f)
+		if err != nil {
+			return err
+		}
+	}
+	{
+		f, err := os.Create("cubic.plonk.pk")
+		if err != nil {
+			return err
+		}
+		_, err = pk.WriteTo(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	{
+		f, err := os.Create("contract_plonk.sol")
+		if err != nil {
+			return err
+		}
+		err = vk.ExportSolidity(f)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+
+	_ = generatePlonk()
+
+}
